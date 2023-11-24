774679b00750d45cf15d1a21066e2e11b8eed5e5
CesaireMonjoly <cesairemonjoly@free.fr>
2023-11-24 22:35:18 +0100

    Scale and Move transform done :p

--- src/main.c
+++ src/main.c
@@ -3,6 +3,12 @@
 #include <graphx.h>
 #include <debug.h>
 
+#include <stdlib.h>
+#include <math.h>
+
+//GFX_LCD_WIDTH
+//GFX_LCD_HEIGHT
+
 struct vec3
 {
     int24_t x;
@@ -23,32 +29,56 @@ struct vec2 screenSpace(struct vec2 CoordinateSpace)
     return CoordinateSpace;
 }
 
-struct vec2 coordinateSpace(struct vec3 CamSpace, int dist)
+struct vec2 coordinateSpace(struct vec3 CamSpace, int Dist)
 {
     struct vec2 CoordinateSpace;
-    CoordinateSpace.x = CamSpace.x*((CamSpace.z + dist)/dist);
-    CoordinateSpace.y = CamSpace.y*((CamSpace.z + dist)/dist);
+    CoordinateSpace.x = CamSpace.x*((CamSpace.z + Dist)/Dist);
+    CoordinateSpace.y = CamSpace.y*((CamSpace.z + Dist)/Dist);
     return CoordinateSpace;
 }
 
-void drawCube(int dist)
+void applyScale(struct vec3 Origin, struct vec3*Point, struct vec3 ScaleVector)
+{
+    int dx = abs(Origin.x - Point->x)*ScaleVector.x;
+    int dy = abs(Origin.x - Point->y)*ScaleVector.y;
+    int dz = abs(Origin.z - Point->z)*ScaleVector.z;
+
+    Point->x = Point->x * dx;
+    Point->y = Point->y * dy;
+    Point->z = Point->z * dz;
+}
+
+void applyMovement(struct vec3 Origin, struct vec3*Point, struct vec3 MovementVector)
+{
+    int dx = abs(Origin.x - Point->x)*MovementVector.x;
+    int dy = abs(Origin.x - Point->y)*MovementVector.y;
+    int dz = abs(Origin.z - Point->z)*MovementVector.z;
+
+    Point->x = Point->x + dx;
+    Point->y = Point->y + dy;
+    Point->z = Point->z + dz;
+}
+
+void drawCube(int Dist, int Angle)
 {
     struct vec3 CamSpace = {0, 0, 0};
     struct vec2 CoordinateSpace = {0, 0};
     struct vec2 ScreenSpace = {0, 0};
-    int color_index = 7;
-    for (int x = 0; x < 4 ; x++)
+    int Color_index = 7;
+    for (int x = -2; x < 3 ; x++)
     {
-        for (int y = 0; y < 4; y++)
+        for (int y = -2; y < 3; y++)
         {
-            color_index = color_index + 8;
-            gfx_SetColor(color_index);
+            Color_index = Color_index + 8;
+            gfx_SetColor(Color_index);
             for (int z = 0; z < 4; z++)
             {
-                CamSpace.x = (x*6)-4;
-                CamSpace.y = (y*6)-4;
-                CamSpace.z = (z*6);
-                CoordinateSpace = coordinateSpace(CamSpace, dist);
+                CamSpace.x = x*8;
+                CamSpace.y = y*8;
+                CamSpace.z = z*8;
+
+
+                CoordinateSpace = coordinateSpace(CamSpace, Dist);
                 ScreenSpace = screenSpace(CoordinateSpace);
                 gfx_SetPixel(ScreenSpace.x, ScreenSpace.y);
             }
@@ -65,13 +95,14 @@ int main(void)
     gfx_Begin();
     gfx_SetDrawBuffer();
     gfx_SetColor(0);
-    int fov = 7;
+    int Fov = 7;
+    int Angle = 0;
 
     do
     {
         gfx_ZeroScreen();
-        drawCube(fov);
-        //gfx_Line(pts1.x, pts1.y, pts2.x, pts2.y);
+        Angle += 0;
+        drawCube(Fov, Angle);
         gfx_SetColor(0);
         gfx_SwapDraw();
     } while (kb_Data[1] != kb_2nd); //exit key
